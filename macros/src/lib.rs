71f1f56a066f1f2e79cbdcbfbbab55bfdf2722dc
dG9hc3Q <dG9hc3Q@icloud.com>
2025-01-11 16:19:51 +1000

    fix: update macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,65 +1,17 @@
-#![feature(proc_macro_span)]
-
 extern crate proc_macro;
-
-use proc_macro::{LineColumn, TokenStream, TokenTree};
+use proc_macro::TokenStream;
 use quote::quote;
 
-struct Source {
-    source: String,
-    line: usize,
-    col: usize,
-}
-
-impl Source {
-    fn reconstruct_from(&mut self, input: TokenStream) {
-        for t in input {
-            if let TokenTree::Group(g) = t {
-                let s = g.to_string();
-                self.add_whitespace(g.span_open().start());
-                self.add_str(&s[..1]); // the '[', '{' or '('.
-                self.reconstruct_from(g.stream());
-                self.add_whitespace(g.span_close().start());
-                self.add_str(&s[s.len() - 1..]); // the ']', '}' or ')'.
-            } else {
-                self.add_whitespace(t.span().start());
-                self.add_str(&t.to_string());
-            }
-        }
-    }
-
-    fn add_str(&mut self, s: &str) {
-        // Let's assume for now s contains no newlines.
-        self.source += s;
-        self.col += s.len();
-    }
-
-    fn add_whitespace(&mut self, loc: LineColumn) {
-        while self.line < loc.line {
-            self.source.push('\n');
-            self.line += 1;
-            self.col = 0;
-        }
-        while self.col < loc.column {
-            self.source.push(' ');
-            self.col += 1;
-        }
-    }
-}
-
-/// Run AppleScript
 #[proc_macro]
 pub fn __applescript(input: TokenStream) -> TokenStream {
-    let mut s = Source {
-        source: String::new(),
-        line: 1,
-        col: 0,
+    let input = input.to_string().replace("\n", " ");
+    let string_literals = input.split(';')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| format!("{}", s));
+    let output = quote! {
+        run_applescript(&[#(#string_literals),*])
     };
-    s.reconstruct_from(input);
-
-    let source = s.source;
-    quote! {
-        run_applescript(#source)
-    }
-    .into()
+    
+    TokenStream::from(output)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 pub use macros::__applescript;
 use std::process::Command;
 
-pub fn run_applescript(script: &str) {
-    let process = Command::new("osascript")
-        .arg("-l")
-        .arg("AppleScript")
-        .arg("-e")
-        .arg(script)
-        .spawn()
-        .expect("Failed to run AppleScript");
+pub fn run_applescript(lines: &[&str]) {
+    println!("Executing script lines: {:?}", lines);
+    
+    let mut cmd = Command::new("osascript");
+    for line in lines {
+        cmd.arg("-e").arg(line);
+    }
 
-    let _ = process.wait_with_output().unwrap();
+    let process = cmd.spawn().expect("Failed to run AppleScript");
+    println!("{:?}", process.wait_with_output().unwrap());
 }
 
 #[macro_export]
@@ -24,25 +24,38 @@ macro_rules! applescript {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn test_open_url() {
-        applescript!(
+        applescript! {
             tell application "Safari" to open location "https://www.rust-lang.org"
-        );
+        }
     }
 
+    /* IMPORTANT: multiline requires each line end with ; */
     #[test]
     fn test_multiline_works() {
         applescript! {
-            tell application "Safari"
-                activate
-                try
-                    tell window 1 to set current tab to make new tab with properties {URL:"https://www.rust-lang.org"}
-                on error
-                    open location theURL
-                end try
-            end tell
+            tell application "Safari";
+                activate;
+                open location "https://www.rust-lang.org";
+                set bounds of front window to {100, 100, 1000, 800};
+            end tell;
         }
     }
+
+    #[test]
+    fn test_list_notes() {
+        applescript! {
+            tell application "Notes";
+                set noteList to "";
+                    repeat with aNote in notes;
+                        set noteList to noteList & name of aNote & linefeed;
+                    end repeat;
+                return noteList;
+            end tell;
+        }
+    }
+
 }
